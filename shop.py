af7e258e6dc393804bcd3a70cd185313cc6491cf
Koko <pointmisser@gmail.com>
2023-03-26 23:50:30 +0100

    Refractor and format code.
    Add validate_confirmation, validate_budget and validate_attempt functions.
    
    Take out checking budget functionality to a separate fuction.

--- shop.py
+++ shop.py
@@ -1,41 +1,47 @@
 
-# 1.	Create a dictionary with a minimum of 3 items and prices
-# a.	One of the items needs to cost more than £100
-stock = {'charger': 11, 'kettle': 25, 'headphones': 75, 'stereo': 140, 'Robot': 99.50}
+"""Shop program"""
 
-# 2.	Customer’s available money is £100
-thank_you_mssg = "Thank you for visiting out shop! Goodbye."
+# A dictionary with a minimum of 3 items and prices
+stock = {'charger': 11, 'kettle': 25,
+         'headphones': 75, 'stereo': 140, 'Robot': 99.50}
+# A thank you message for the customer
+thank_you_mssg = "Thank you for visiting our shop! Goodbye."
 
-# Custom error
-class TooManyAttempts(ValueError):
+# Custom errors
+
+
+class BudgetTooLow(ValueError):
     """Raise when user tries to purches item more than 3 times."""
-    pass
 
-# 3.	Welcome the customer and display the items and their prices, along with an option to “exit”
+
+class TooManyAttempts(Exception):
+    """Raise error when attempts are higher than 3"""
+
+# Welcome the customer and display the items and their prices, along with an option to “exit”
+
+
 def setup(items):
     """Print welcome and display times in the shop"""
     print("Welcome to the shop!")
     print("What we have currently in stock: \n")
     # print table headers
-    print("Opt. |".rjust(4) + "Item".ljust(12) +"| Price")
-    print("-" * 5 + "|" + "-" * 12 + "|"+ "-" * 8 )
-    # go over items and add them to a list along with their option to use for user input
+    print("Opt. |".rjust(4) + "Item".ljust(12) + "| Price")
+    print("-" * 5 + "|" + "-" * 12 + "|" + "-" * 8)
+    # go over items and add them to a list along with their option to use in user input
     item_list = []
-    for i, (name, price) in enumerate(items.items(),1):
+    for i, (name, price) in enumerate(items.items(), 1):
         item_list.append({name: price})
         print(f"{i:>4} | {name:<10} | {price:.2f}")
-    item_list.append("Exit shop?" )
+    item_list.append("Exit shop?")
     print("-" * 27)
     print(f"{len(item_list):>4} | Exit shop? \n")
     return item_list
 
-# 4.	Accept the option as an input, an invalid input should raise a ValueError
+# Validation functions
 
 
 def validate_if_in_range(user_input, item_list):
-    """Get user's selection"""
-    # if user_input.isalpha() or user_input.isalnum() and not user_input.isdigit():
-    #     raise ValueError("Invalid input. Your selection must be a number")
+    """Check if user's input is in list range"""
     user_input = int(user_input)
     if user_input not in range(1, len(item_list)+1):
         raise ValueError(f"We do not seem to have this item in stock.\
@@ -43,10 +49,30 @@ def validate_if_in_range(user_input, item_list):
 
 
 def validate_if_number(user_input):
-    """Validate if input is a number"""
+    """Check if user's input is a number"""
     if user_input.isalpha() or user_input.isalnum() and not user_input.isdigit():
         raise ValueError("Invalid input. Your selection must be a number")
 
+
+def validate_confirmation(user_input):
+    """Check if y/n confirmation is valid and exit if n"""
+    if user_input not in ['y', 'n']:
+        raise ValueError("Invalid input. Please type in 'y' or 'n'.")
+    if user_input == 'n':
+        quit()
+
+
+def validate_budget(budget, price):
+    """Check if budget is higher than the price"""
+    if budget < price:
+        raise BudgetTooLow("You do not have enough money to buy this item.")
+
+
+def validate_attempts(attempt):
+    """Check numbe of attempts"""
+    if attempt == 3:
+        raise TooManyAttempts(
+            "You many unsuccessful attempts. Try again later")
 
 def make_selection(item_list):
     """Ask user to select the item"""
@@ -55,59 +81,62 @@ def make_selection(item_list):
         try:
             validate_if_number(option)
             validate_if_in_range(option, item_list)
-            option = int(option)
-            selection = item_list[option-1]
         except ValueError as error:
             print(F"{error} Try again")
             continue
+        else:
+            option = int(option)
+            selection = item_list[option-1]
         return selection
 
-# If the customer can afford it, print out a message saying “Here’s your {item}!”
-# 6.	The user should be then greeted out of the shop, and the program terminated.
+def check_budget(selection):
+    """Check if user's purchase is within budget"""
+    customer_budget = 100
+    attempt = 1
+    for k, v in selection.items():
+        item = k
+        price = v
+    try:
+        validate_budget(customer_budget, price)
 
-# 7.	If the customer cannot afford it, note the attempt and ask if they have more money, if they do and enter the amount it should be added to the balance.
-# 8.	The purchase should be tried a maximum of 3 items, if it fails a custom error should be raised and the customer will exit the shop.
+    except BudgetTooLow as error:
+        print(error)
+        # if price > customer_budget:
+        while True:
+            try:
+                increase_budget = input(
+                    "Do you want increase your budget? y/n ").lower()
+                validate_confirmation(increase_budget)
+                extra_budget = input(
+                    "Please type by how much more you can spend: ")
+                validate_if_number(extra_budget)
+                attempt += 1
+                customer_budget += int(extra_budget)
+                print(f"You inreased your budget to £{customer_budget}")
+                validate_attempts(attempt)
+                validate_budget(customer_budget, price)
+                print(
+                    f"Congratulations on your purchase. Here’s your item: \n {item.upper()} - £{price}")
+                break
+            except ValueError as v_error:
+                print(v_error)
+            except TooManyAttempts as c_error:
+                print(c_error)
+                break
+
+    else:
+        print(
+            f"Congratulations on your purchase. Here’s your item: \n {item.upper()} - £{price}")
+    finally:
+        print(thank_you_mssg)
+        quit()
 
 
 def run_shop():
     """Run shop program"""
     options = setup(stock)
-    customer_budget = 100
     selection = make_selection(options)
-    if selection == len(options):
-        print(thank_you_mssg)
-        quit()
-    for k, v in selection.items():
-        item = k
-        value = v
-        if value <= customer_budget:
-            print(f"Here’s your {item}")
-            break
-        else:
-            n = 1
-            while n < 2:
-                increase_budget = ""
-                while increase_budget not in ['y', 'n']:
-                    increase_budget = input(
-                        "You cannot afford this item. Can you increase your budget? y/n ").lower()
-                    continue
-                if increase_budget == 'n':
-                    print(thank_you_mssg)
-                    quit()
-                else:
-                    extra_budget = input(
-                        "Please type by how much more you can spend: ")
-                    validate_if_number(extra_budget)
-                    customer_budget += int(extra_budget)
-                    if value > customer_budget:
-                        n += 1
-                        print(
-                            f"Your budget is increased to £{customer_budget}, but it still not enough.")
-                        continue
-                    else:
-                        print(f"Here’s your {item}")
-                        break
-            raise TooManyAttempts(
-                "You tried too many times, try again later.")
+    check_budget(selection)
+
 
 run_shop()
