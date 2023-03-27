3e8c380c533a5601c1e1eaee585c9de0f9ffa672
Koko <pointmisser@gmail.com>
2023-03-27 21:54:27 +0100

    Add test for validate_budget and validate_if_number with negative input.

--- test_shop.py
+++ test_shop.py
@@ -4,7 +4,8 @@ import unittest
 from shop import(setup,
     validate_if_in_range,
     validate_if_number,
-    validate_confirmation)
+    validate_confirmation,
+    validate_budget)
 
 
 class TestShop(unittest.TestCase):
@@ -22,10 +23,16 @@ class TestShop(unittest.TestCase):
         user_input = 9
         with self.assertRaises(ValueError):
             validate_if_in_range(user_input, self.stock)
-            
+
     def test_validate_if_number(self):
-        """Test number validation"""
+        """Test number validation when input is not a number"""
         user_input = "l"
+        with self.assertRaises(ValueError):
+            validate_if_number(user_input)
+    
+    def test_validate_if_negative_number(self):
+        """Test number validation when input is negative"""    
+        user_input = "-2"
         with self.assertRaises(ValueError):
             validate_if_number(user_input)
 
@@ -34,13 +41,21 @@ class TestShop(unittest.TestCase):
         user_input = 0
         with self.assertRaises(ValueError):
             validate_confirmation(user_input)
-            
+
     def test_validate_confirmation_if_no(self):
         """Test if program exits if input == n"""
         user_input = "n"
         with self.assertRaises(SystemExit) as error:
             validate_confirmation(user_input)
             self.assertEqual(error.exception.code, 1)
-            
+
+    def validate_budget(self):
+        """Test if budget is lower than item price"""
+        budget = 10
+        item_price = 20
+        with self.assertRaises(ValueError):
+            validate_budget(budget, item_price)
+
+    
 if __name__ == '__main__':
     unittest.main()
